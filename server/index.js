219d71e2a812c3df4ddcbe67e04ea9c33c15c35c
sleepytmzd <121752215+sleepytmzd@users.noreply.github.com>
2024-02-14 20:25:08 +0600

    Added apis

--- server/index.js
+++ server/index.js
@@ -18,8 +18,159 @@ app.use(express.json());
 
 async function run (){
     try{
-        console.log("hello ");
         
+
+        // Create project
+        app.post('./project', async (req, res) => {
+            try {
+                
+                const creator_id = req.body.creator_id;
+                const category = req.body.category;
+                const name = req.body.name;
+                const description = req.body.description;
+
+                await pool.query(
+                    `
+                    INSERT INTO project (creator_id, category, name, description) VALUES ($1, $2, $3, $4)
+                    `, [creator_id, category, name, description]
+                );
+
+                res.json({message: `Project added`});
+
+            } catch (error) {
+                console.error('error executing query: ', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+        // Get all projects
+        app.get('./project', async(req, res) => {
+            try {
+                
+                const projects = await pool.query(
+                    `
+                    SELECT * FROM project
+                    `
+                );
+
+                res.json(projects.rows);
+
+            } catch (error) {
+                console.error('error executing query: ', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+        // Get a particular project
+        app.get('./project/:id', async(req, res) => {
+            try {
+                
+                const id = req.params.id;
+
+                const project = await pool.query(
+                    `
+                    SELECT * FROM project
+                    WHERE id = $1
+                    `, [id]
+                );
+
+                res.json(project.rows[0]);
+
+            } catch (error) {
+                console.error('error executing query: ', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+        // Get all projects under a particular user
+        app.get('./user/:id/project', async(req, res) => {
+            try {
+                
+                const id = req.params.id;
+
+                const projects = await pool.query(
+                    `
+                    SELECT * FROM project
+                    WHERE creator_id = $1
+                    `, [id]
+                );
+
+                res.json(projects.rows);
+
+            } catch (error) {
+                console.error('error executing query: ', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+        // invite collaborators
+        app.post('./project/invite', async(req, res) => {
+            try {
+                
+                const project_id = req.body.project_id;
+                const user_id = req.body.user_id;
+                const is_approved = "INVITED";
+
+                await pool.query(
+                    `
+                    INSERT INTO project_user (project_id, user_id, is_approved) VALUES ($1, $2, $3)
+                    `, [project_id, user_id, is_approved]
+                );
+
+                res.json({message: `Invited`});
+
+            } catch (error) {
+                console.error('error executing query: ', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+        // Approve collaborator
+        app.post('./project/invite/approve', async(req, res) => {
+            try {
+                
+                const project_id = req.body.project_id;
+                const user_id = req.body.user_id;
+                
+                await pool.query(
+                    `
+                    UPDATE project_user SET is_approved = APPROVED
+                    WHERE project_id = $1 AND user_id = $2
+                    `, [project_id, user_id]
+                );
+
+                res.json({message: `Approved`});
+
+            } catch (error) {
+                console.error('error executing query: ', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+        // Request to collaborate
+        app.post('./project/request', async(req, res) => {
+            try {
+        
+                const project_id = req.body.project_id;
+                const user_id = req.body.user_id;
+                const is_approved = "REQUESTED";
+
+                await pool.query(
+                    `
+                    INSERT INTO project_user (project_id, user_id, is_approved) VALUES ($1, $2, $3)
+                    `, [project_id, user_id, is_approved]
+                );
+
+                res.json({message: `Requested`});
+
+            } catch (error) {
+                console.error('error executing query: ', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        })
+
+
+
     }finally{
 
     }
