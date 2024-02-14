8dd12ec011cd0b0c1a92a6178641ab5a8cec7d29
Mehemud Azad <mr.x@MehemudAzad.local>
2024-02-14 18:30:51 +0600

    hello server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('./src/config/db');
+const pool = require('./db');
 // const bcrypt = require('bcrypt');
 
-const port = process.env.PORT || 5002;
-const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5003;
+// const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 const app = express();
 
+
+
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+async function run (){
+    try{
+        console.log("hello ");
+
+    }finally{
+
+    }
+}
+
+//running the function
+run().catch(err => console.error(err));
+
 //listening to the port
 app.listen(port, () => {
     console.log(`test server running on ${port}`);
